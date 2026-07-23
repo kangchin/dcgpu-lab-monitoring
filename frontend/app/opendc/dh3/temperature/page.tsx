0dce0b6a2f4bb4b57debc8215d38ba1f6ba29ceb
aikhosaw <aikhong.saw@amd.com>
2026-07-23 14:32:01 +0800

    Update path

---
+++ frontend/app/opendc/dh3/temperature/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { DH3TemperatureMonitor } from "@/components/dh3-temperature-monitor";
+
+export default function DH3TemperaturePage() {
+  return (
+    <div className="w-full">
+      <DH3TemperatureMonitor />
+    </div>
+  );
+}
